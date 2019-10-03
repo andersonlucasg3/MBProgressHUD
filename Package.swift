5c6b1e7e341f26433832b6faea2a2408a30636e2
Anderson Lucas C. Ramos <anderson@Andersons-MacBook-Pro.local>
2019-10-03 16:03:45 -0300

    Changing structure of the Package;

--- Package.swift
+++ Package.swift
@@ -4,15 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "MBProgressHUD",
-    products: [
-        .library(name: "MBProgressHUD", targets: ["MBProgressHUD"])
-    ],
-    targets: [
-        .target(
-            name: "MBProgressHUD",
-            path: ".",
-            exclude: ["Demo"],
-            sources: ["MBProgressHUD.h", "MBProgressHUD.m"]
-        )
-    ]
+    products: [ .library(name: "MBProgressHUD", targets: ["MBProgressHUD"]) ],
+    targets: [ .target(name: "MBProgressHUD") ]
 )
